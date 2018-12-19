0cdd445064d6729007d327f6b77a59eee55b22d4
9pid <>
2018-12-19 17:01:26 +0900

    履歴を追えるよに修正

--- src/index.js
+++ src/index.js
@@ -54,12 +54,15 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      // 現在のステップ数
+      stepNumber: 0,
+      // 現在のプレイヤーがXかどうか
       xIsNext: true
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -74,15 +77,38 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      // 現在のステップ数
+      stepNumber: history.length,
       // 選手交代
       xIsNext: !this.state.xIsNext
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    // 履歴を追うためのボタン
+    const moves = history.map((_, step) => {
+      // 遷移先メッセージ
+      const message = step? ('Go to move #' + step): 'Go to game start';
+
+      return (
+        <li key={step}>
+          <button onClick={() => this.jumpTo(step)}>
+            {message}
+          </button>
+        </li>
+      );
+    });
 
     const status = ((winner, xIsNext) => {
       // 勝利プレイヤーがいれば表示
@@ -100,7 +126,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
