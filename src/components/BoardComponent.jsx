d51f6cf19c663e0334a42e778dfc4ec67f1da053
kanehira <kanehira@membera-no-MacBook-Air.local>
2018-12-19 17:16:46 +0900

    部品をファイルに分割

---
+++ src/components/BoardComponent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import SquareComponent from './SquareComponent';
+
+export default class BoardComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+  renderSquare(i) {
+    return <SquareComponent value={this.props.squares[i]}
+                            onClick={() => this.props.onClick(i)} />;
+  }
+}
